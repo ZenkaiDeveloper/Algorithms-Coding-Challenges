4cb9cac35a04ebdbe0a692dfdb86f8da8135afae
Andy <wang.andy32@gmail.com>
2019-01-13 20:29:53 -0500

    binary search tree

---
+++ Data Structures/BinarySearchTree.js
@@ -0,0 +1,74 @@
+class Node {
+  constructor(value){
+    this.value = value,
+    this.right = null,
+    this.left = null
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null;
+  }
+
+  insert(value){
+    let newNode = new Node(value);
+    let temp = this.root;
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }else {
+    while(temp){
+      if (value === temp.value) {
+        return undefined;
+      }
+      else if (value < temp.value) {
+        if(!temp.left){
+          temp.left = newNode;
+          return this;
+        }else{
+          temp = temp.left;
+        }
+      }else if (value > temp.value) {
+        if (!temp.right) {
+          temp.right = newNode;
+          return this;
+        }else{
+          temp = temp.right
+        }
+      }
+    }
+
+    }
+  }
+
+
+  find(value){
+    let current = this.root;
+    if (!this.root) {
+      return null;
+    }else if (this.root.value === value) {
+      return true;
+    }else if (current.value > value) {
+      while (current.left) {
+        if (value === current.left.value) {
+          return true
+        }else{
+          current = current.left;
+        }
+      }
+      return false;
+    }else if (current.value < value) {
+      while (current.right) {
+        if (current.right.value === value) {
+          return true;
+        }else{
+          current = current.right;
+        }
+      }
+      return false
+    }
+  }
+
+
+}
